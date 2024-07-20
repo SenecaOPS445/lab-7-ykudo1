71aad4874a339f30a0eca4d69eacc847e0d3729e
ykudo1 <ykudo@myseneca.ca>
2024-07-20 00:54:04 -0400

    completed lab7a

--- lab7a.py
+++ lab7a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id]
+# Student ID: Yuna Kudo
 class Time:
     """Simple object type for time of the day.
     data attributes: hour, minute, second
@@ -20,6 +20,13 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+
+    if sum.second > 60:
+        sum.minute += 1
+        sum.second -= 60
+    elif sum.minute > 60:
+        sum.hour += 1
+        sum.minute -= 60
     return sum
 
 def valid_time(t):
